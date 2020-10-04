001835193530a4eddbd3a3b72cc7935afffb0916
vietbach-kt <kubach10x@gmail.com>
2020-10-05 06:08:42 +0700

    commit 1

---
+++ 3.PROJECT/ManageUniversity/app/Http/Controllers/HomepagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomepagesController extends Controller
+{
+    //
+    public function home()
+    {
+        return view('welcome');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+}
---
+++ 3.PROJECT/ManageUniversity/resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+<h3>Local host</h3>
